5096f984b53a00a3046c658a71c6f68e46b04bf2
Emanuel Rivas <Emanuelrivasdavid@gmail.com>
2023-09-07 15:57:50 -0400

    Clarified Instructions inside script

--- email_messenger.py
+++ email_messenger.py
@@ -17,11 +17,12 @@ email_receivers = data.split("\n")
 print(email_receivers)
 my_file.close()
 
-
+# THESE ARE THE FIELDS WHERE THE SUBJECT AND YOUR EMAIL BODY ARE ENTERED
 subject = 'ENTER SUBJECT HERE'
 body = """
    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 """
+
 
 
 #IF YOU DO NOT WISH TO SEND ANY PDF, COMMENT OUT LINES 29 AND 30 AS WELL AS LINES 43-44
